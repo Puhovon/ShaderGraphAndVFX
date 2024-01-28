538005e570e18a1dcfade3ecca676afaff12b8e8
Arkadiy <91030077+Puhovon@users.noreply.github.com>
2024-01-29 01:38:02 +0300

    10

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using DefaultNamespace;
+using UnityEngine;
+using UniRx;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private int health;
+    [SerializeField] private int damage;
+    [SerializeField] private int level;
+    [SerializeField] private int armor;
+    [SerializeField] private int spellResis;
+
+    private PlayerStats stats;
+    private void Start()
+    {
+        var uniStats = Observable.Start(GetStats);
+        Debug.Log(GetStats());
+
+        Observable.WhenAll(uniStats).ObserveOnMainThread().Subscribe(xs =>
+        {
+            Debug.Log(xs.Length);
+            Debug.Log(xs[0]);
+            stats = JsonUtility.FromJson<PlayerStats>(xs[0]);
+            Debug.Log(stats);
+            health = stats.health;
+            damage = stats.damage;
+            level = stats.level;
+            armor = stats.armor;
+            spellResis = stats.spellResist;
+        });
+        
+    }
+
+    private string GetStats()
+    {
+        string jsonPath = "./Assets/Scripts/PlayerConfig.json";
+        var json = File.ReadAllText(jsonPath);
+        return json;
+    }
+
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,20 @@
+﻿namespace DefaultNamespace
+{
+    public struct PlayerStats
+    {
+        public int health;
+        public int damage;
+        public int level;
+        public int armor;
+        public int spellResist;
+
+        public PlayerStats(int health, int damage, int level, int armor, int spellResist)
+        {
+            this.health = health;
+            this.damage = damage;
+            this.level = level;
+            this.armor = armor;
+            this.spellResist = spellResist;
+        }
+    }
+}
